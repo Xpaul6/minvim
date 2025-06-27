5144a00b4aaf0c9d6fd87ccc3337f2ddbe77e27a
Pavel Kholkin <xpaul6@gmail.com>
2025-06-27 13:00:37 +0500

    moved from packer to lazy.nvim

---
+++ lua/minvim/lazy.lua
@@ -0,0 +1,64 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
+vim.g.maplocalleader = "\\"
+
+require("lazy").setup({
+  spec = {
+    {
+      "folke/lazy.nvim",
+      version = "*",
+      lazy = false,
+    },
+    {
+      "nyoom-engineering/oxocarbon.nvim",
+      config = function()
+        vim.cmd("colorscheme oxocarbon")
+      end,
+    },
+    {
+      "nvim-telescope/telescope.nvim",
+      tag = "0.1.8",
+      dependencies = {
+        { "nvim-lua/plenary.nvim" },
+      },
+    },
+    {
+      "nvim-treesitter/nvim-treesitter",
+      build = ":TSUpdate",
+    },
+    {
+      "windwp/nvim-autopairs",
+      event = "InsertEnter",
+      config = function()
+        require("nvim-autopairs").setup {}
+      end,
+    },
+    {"lewis6991/gitsigns.nvim"},
+    { "neovim/nvim-lspconfig" },
+    { "hrsh7th/nvim-cmp" },
+    { "hrsh7th/cmp-nvim-lsp" },
+    { "onsails/lspkind.nvim" },
+    {
+      "L3MON4D3/LuaSnip",
+    },
+    { "williamboman/mason.nvim" },
+    { "williamboman/mason-lspconfig.nvim" },
+  },
+  install = { colorscheme = { "habamax" } },
+  checker = { enabled = true },
+})
+
